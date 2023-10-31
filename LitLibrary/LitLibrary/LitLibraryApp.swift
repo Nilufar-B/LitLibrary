345c59101ce6fbe042bc1b50dffcdc056121c9bb
Nilufar-B <112411542+Nilufar-B@users.noreply.github.com>
2023-10-31 13:27:08 +0100

    project created

---
+++ LitLibrary/LitLibrary/LitLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LitLibraryApp.swift
+//  LitLibrary
+//
+//  Created by Nilufar Bakhridinova on 2023-10-31.
+//
+
+import SwiftUI
+
+@main
+struct LitLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
